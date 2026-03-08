7c4736267ec755f48d3b1f8a9b7f87203229d283
Fab1810 <Davidcontacto23@gmail.com>
2026-03-07 21:09:51 -0500

    Semana 1 - Ejercicio1

---
+++ Semana1/Ejercicio1.py
@@ -0,0 +1,30 @@
+#promediodenotas
+num1=float (input("Ingrese la primera nota"))
+num2=float (input("Ingrese la segunda nota"))
+num3=float (input("Ingrese la tercera nota"))
+
+promedio=(num1+num2+num3)/3
+
+print("El promedio de las tres notas es",promedio)
+
+#semana1.py
+#Este es mi Comentario 
+#Uso de print para mostrar mensajes en la pantalla 
+#Uso de input para solicitar datos al usuario 
+
+print ("Bienvenido a la clase de python")
+Minombre=input ("¿Cual es su  nombre?")
+Apellido=input ("¿Cual es su apellido?")
+print ("Hola", Minombre, Apellido, "Bienvenido a la clase")
+#uso de variables para almacenar datos
+num1=30
+num2=10.2
+name="fabian sanchez"
+valor1=True
+valor2=False
+print (valor1)
+print(valor2)
+a,b,c=20,40,60
+print (a)
+print (b)
+print (c)
