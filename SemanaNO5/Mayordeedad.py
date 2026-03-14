228e9f0f8812832b82b2d21d3b9fb714c3ab8af2
Fab1810 <Davidcontacto23@gmail.com>
2026-03-13 21:38:17 -0500

    Semana5

---
+++ SemanaNO5/Mayordeedad.py
@@ -0,0 +1,14 @@
+edad=int(input("Dame la edad "))
+if edad>=18:
+   print ("Puedes entrar a la discoteca")
+else:
+  print ("No puede entrar a la discoteca")
+
+#Cuanto es la nota
+
+nota=float(input("Dame la nota"))
+if nota >3.0:
+  print ("Paso la materia")
+else:
+  print ("No paso la  materia")
+ 
